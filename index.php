d7a07679a5f00a654d39f8ddb03e0cab12ca321a
unknown <nikola94lukic@hotmail.com>
2019-04-13 22:27:40 +0200

    first commit - navbar

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" contect="width=device-width, initial-scale=1"> 
+	<title>My Store</title>
+	<link rel="stylesheet" href="styles/bootstrap-337.min.css">
+	<link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+<div id="top"><!-- Top - start -->
+	<div class="container"><!-- Container - start -->
+		<div class="col-md-6 offer"><!-- col-md-6 offer - start -->
+			<a href="" class="btn btn-success btn-sm">Welcome</a>
+			<a href="checkout.php">2 Items In Your Cart | Total Price: $400</a>
+		</div><!-- col-md-6 offer - end -->
+		<div class="col-md-6"><!-- col-md-6  - start -->
+			<ul class="menu">
+				<li>
+					<a href="cutomer_register.php">Register</a>
+				</li>
+				<li>
+					<a href="checkout.php">My Account</a>
+				</li>
+				<li>
+					<a href="cart.php">Go To Cart</a>
+				</li>
+				<li>
+					<a href="login.php">Login</a>
+				</li>
+			</ul>
+		</div><!-- col-md-6  - end -->
+	</div><!-- Container - end -->
+
+</div><!-- Top - end -->
+
+	<script src="js/jquery-331.min.js"></script>
+	<script src="js/bootstrap-337.min.js"></script>
+</body>
+</html>
